baf7b6c4c4f118e58f1d87cb087f3c89bd3d4a63
DeeNeoo <ST10490306@imconnect.edu.za>
2025-05-21 12:44:10 +0200

    AssignmentTwo

--- app/src/main/java/za/ac/iie/assignmenttwoo/MainActivity4.kt
+++ app/src/main/java/za/ac/iie/assignmenttwoo/MainActivity4.kt
@@ -2,6 +2,7 @@ package za.ac.iie.assignmenttwoo
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
 import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -20,13 +21,15 @@ class MainActivity4 : AppCompatActivity() {
         val questions = intent.getStringArrayListExtra("QUESTIONS")
         val answers = intent.getBooleanArrayExtra("ANSWERS")
         val reviewLayout = findViewById<LinearLayout>(R.id.reviewLayout)
+        val btnExit = findViewById<Button>(R.id.btnExit)
 
 
         // Build a string to display all questions and their correct answers
         if (questions != null && answers != null) {
             for (i in questions.indices) {
                 val questionText = TextView(this)
-                questionText.text = "${i + 1}. ${questions[i]} - ${if (answers[i]) "Correct" else "Incorrect"}"
+                questionText.text =
+                    "${i + 1}. ${questions[i]} - ${if (answers[i]) "Correct" else "Incorrect"}"
                 questionText.setTextColor(
                     resources.getColor(
                         if (answers[i]) android.R.color.holo_green_dark else android.R.color.holo_red_dark,
@@ -39,13 +42,15 @@ class MainActivity4 : AppCompatActivity() {
             }
         }
 
+        btnExit.setOnClickListener {
+            finishAffinity() // Closes all activities and exits the app
 
 
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
         }
     }
 }
